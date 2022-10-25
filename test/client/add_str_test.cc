a58c673e8f97bfd8967348de1f5bba4cede1f4cf
Conzxy <1967933844@qq.com>
2022-10-25 13:00:17 +0800

    third-party

--- test/client/add_str_test.cc
+++ test/client/add_str_test.cc
@@ -1,5 +1,8 @@
-#include "mmkv/mmkv.h"
+#include "mmkvc/mmkv.h"
 
+#include "kanon/thread/count_down_latch.h"
+
+#include <iostream>
 #include <stdio.h>
 #include <string>
 
@@ -8,22 +11,23 @@
 using namespace mmkv;
 
 int main() {
-  MmkvInit();
-
   Mmkv cli("127.0.0.1:9998");
 
-  bool res[N];
+  kanon::CountDownLatch latch(N);
   for (int i = 0; i < N; ++i) {
-    cli.AddStr(std::to_string(i), std::to_string(i), &res[i]);
+    cli.AddStr(std::to_string(i), std::to_string(i), [i, &latch](MmkvValue &value) {
+      latch.Countdown();
+      std::cout << "[" << i << "]: ";
+      if (value.status_code() == mmkv::protocol::S_OK) {
+        std::cout << "True\n";
+      } else {
+        std::cout << "False\n";
+      }
+    });
   }
-
-  cli.Wait();
-
-  for (int i = 0; i < N; ++i) {
-    if (res[i])
-      printf("Insert %d successfully\n", i);
-    else 
-      printf("Failed to insert %d\n", i);
-  }
+  
+  std::cout << "Wait async call complete...\n";
+  latch.Wait();
+  std::cout << "All async call Complete\n";
   cli.Close();
 }
